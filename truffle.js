5cc305d74f758b398b20f12d03f0d93f9fbb8bcb
Jack Wu <jackwzp@gmail.com>
2018-10-10 14:46:10 -0400

    Initial Commit

---
+++ truffle.js
@@ -0,0 +1,23 @@
+let HDWalletProvider = require('truffle-hdwallet-provider');
+let mnemonic = "spend tooth cherry broken sound animal cat ozone catalog kitten special outside";
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    buddy: {
+      host: "ganache",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/9f4121fa151f4f51a16bb2444bb9475d");
+      },
+      network_id: '4',
+    }
+  }
+};
